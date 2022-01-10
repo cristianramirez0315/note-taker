0d587f2f09d7a00e70d4d3830eea827420efa6f5
Cristian Ramirez <cristianramirez0315@gmail.com>
2022-01-10 02:37:18 -0500

    deploy

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -6,7 +6,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'))
 })
 
-app.get('/notes', (req, res) => {
+app.get('/notesPage', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
